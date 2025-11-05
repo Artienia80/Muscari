5a78fb2b86ef1e43b9c10ec72e6143889a21a5c7
Artienia80 <87064705+Artienia80@users.noreply.github.com>
2025-11-05 14:08:21 +0300

    Initialize fateEvents and fateDurations data
    Added events and duration periods for the timeline.

---
+++ fateevents.js
@@ -0,0 +1,243 @@
+// Events Timeline Data
+window.fateEvents = [
+    {
+        date: '2025-01-22',
+        dateDisplay: 'January 22, 2025',
+        title: 'First Meeting',
+        milestone: false,
+        current: false
+    },
+    {
+        date: '2025-02-01',
+        dateDisplay: 'February 1, 2025',
+        title: 'Asking Out',
+        milestone: false,
+        current: false
+    },
+    {
+        date: '2025-02-14',
+        dateDisplay: 'February 14, 2025',
+        title: 'First Date',
+        milestone: true,
+        current: false
+    },
+    {
+        date: '2025-03-01',
+        dateDisplay: 'March 1, 2025',
+        title: 'First Unseiso Experience',
+        milestone: false,
+        current: false
+    },
+    {
+        date: '2025-03-14',
+        dateDisplay: 'March 14, 2025',
+        title: 'Official Relationship',
+        milestone: true,
+        current: false
+    },
+    {
+        date: '2025-03-22',
+        dateDisplay: 'March 22, 2025',
+        title: 'Redo Day / Move In Together',
+        milestone: false,
+        current: false
+    },
+    {
+        date: '2025-04-01',
+        dateDisplay: 'April 1, 2025',
+        title: 'Trip to Budapest (I)',
+        milestone: false,
+        current: false
+    },
+    {
+        date: '2025-04-09',
+        dateDisplay: 'April 9, 2025',
+        title: 'Back from Budapest (I)',
+        milestone: false,
+        current: false
+    },
+    {
+        date: '2025-06-05',
+        dateDisplay: 'June 5, 2025',
+        title: 'End of Fabriken Move-In',
+        milestone: false,
+        current: false
+    },
+    {
+        date: '2025-06-17',
+        dateDisplay: 'June 17, 2025',
+        title: 'Trip to Budapest (II)',
+        milestone: false,
+        current: false
+    },
+    {
+        date: '2025-06-23',
+        dateDisplay: 'June 23, 2025',
+        title: 'Trip to Belgrade',
+        milestone: false,
+        current: false
+    },
+    {
+        date: '2025-06-26',
+        dateDisplay: 'June 26, 2025',
+        title: 'Start of Summer LDR',
+        milestone: false,
+        current: false
+    },
+    {
+        date: '2025-08-06',
+        dateDisplay: 'August 6, 2025',
+        title: 'End of Summer LDR, Trip to Budapest (III)',
+        milestone: false,
+        current: false
+    },
+    {
+        date: '2025-08-14',
+        dateDisplay: 'August 14, 2025',
+        title: 'Trip to Poland!',
+        milestone: false,
+        current: false
+    },
+    {
+        date: '2025-08-16',
+        dateDisplay: 'August 16, 2025',
+        title: 'Back from Poland',
+        milestone: false,
+        current: false
+    },
+    {
+        date: '2025-09-05',
+        dateDisplay: 'September 5, 2025',
+        title: 'Ms. Muscari\'s 21st Birthday',
+        milestone: true,
+        current: false
+    },
+    {
+        date: '2025-09-06',
+        dateDisplay: 'September 6, 2025',
+        title: 'Start of Turkey LDR (I)',
+        milestone: false,
+        current: false
+    },
+    {
+        date: '2025-10-24',
+        dateDisplay: 'October 24, 2025',
+        title: 'End of Turkey LDR (I), Trip to Stockholm',
+        milestone: false,
+        current: false
+    },
+    {
+        date: '2025-10-29',
+        dateDisplay: 'October 29, 2025',
+        title: 'End of Stockholm, Start of Turkey LDR (II)',
+        milestone: false,
+        current: false
+    },
+    {
+        date: '2025-11-05',
+        dateDisplay: 'November 5, 2025',
+        title: 'Right Now',
+        milestone: false,
+        current: true
+    }
+];
+
+// Duration Periods Data
+window.fateDurations = [
+    {
+        name: 'Jönköping I',
+        start: '2025-01-22',
+        end: '2025-04-01',
+        displayStart: 'Jan 22',
+        displayEnd: 'Apr 1',
+        ongoing: false
+    },
+    {
+        name: 'Budapest I',
+        start: '2025-04-01',
+        end: '2025-04-09',
+        displayStart: 'Apr 1',
+        displayEnd: 'Apr 9',
+        ongoing: false
+    },
+    {
+        name: 'Jönköping II',
+        start: '2025-04-09',
+        end: '2025-06-17',
+        displayStart: 'Apr 9',
+        displayEnd: 'Jun 17',
+        ongoing: false
+    },
+    {
+        name: 'Budapest II',
+        start: '2025-06-17',
+        end: '2025-06-26',
+        displayStart: 'Jun 17',
+        displayEnd: 'Jun 26',
+        ongoing: false
+    },
+    {
+        name: 'Serbia',
+        start: '2025-06-23',
+        end: '2025-06-25',
+        displayStart: 'Jun 23',
+        displayEnd: 'Jun 25',
+        ongoing: false
+    },
+    {
+        name: 'Summer Long Distance',
+        start: '2025-06-26',
+        end: '2025-08-06',
+        displayStart: 'Jun 26',
+        displayEnd: 'Aug 6',
+        ongoing: false
+    },
+    {
+        name: 'Budapest III',
+        start: '2025-08-06',
+        end: '2025-08-14',
+        displayStart: 'Aug 6',
+        displayEnd: 'Aug 14',
+        ongoing: false
+    },
+    {
+        name: 'Poland',
+        start: '2025-08-14',
+        end: '2025-08-16',
+        displayStart: 'Aug 14',
+        displayEnd: 'Aug 16',
+        ongoing: false
+    },
+    {
+        name: 'Jönköping III',
+        start: '2025-08-16',
+        end: '2025-09-06',
+        displayStart: 'Aug 16',
+        displayEnd: 'Sep 6',
+        ongoing: false
+    },
+    {
+        name: 'Turkey LDR I',
+        start: '2025-09-06',
+        end: '2025-10-24',
+        displayStart: 'Sep 6',
+        displayEnd: 'Oct 24',
+        ongoing: false
+    },
+    {
+        name: 'Stockholm',
+        start: '2025-10-24',
+        end: '2025-10-29',
+        displayStart: 'Oct 24',
+        displayEnd: 'Oct 29',
+        ongoing: false
+    },
+    {
+        name: 'Turkey LDR II',
+        start: '2025-10-29',
+        end: null,
+        displayStart: 'Oct 29',
+        displayEnd: 'Present',
+        ongoing: true
+    }
+];
